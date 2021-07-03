d53cee0499ca48725bea9c3791dddf6992de5a5b
HaythemNajjar <Haythem.najjar@esprit.tn>
2021-07-03 02:44:28 +0100

    test/java/com/softib/TransactionManager/SoftibTransactionManagerApplicationTests.java

---
+++ src/test/java/com/softib/TransactionManager/SoftibTransactionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softib.TransactionManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftibTransactionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
